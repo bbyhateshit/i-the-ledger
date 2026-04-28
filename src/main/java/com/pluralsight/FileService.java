7dd05be9047c7d12f33aba07ecefc5f94b5fe334
bbyhateshit <j.galeta@gmail.com>
2026-04-28 09:13:30 -0400

    imported my list and array list classes which will read and eventually capture the data from user input.

--- src/main/java/com/pluralsight/FileService.java
+++ src/main/java/com/pluralsight/FileService.java
@@ -1,6 +1,19 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileService {
 
     private static final String FILE_PATH = "src/main/resources/transactions.csv";
+
+    public static List<Transaction> readTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+       }
 }
+
