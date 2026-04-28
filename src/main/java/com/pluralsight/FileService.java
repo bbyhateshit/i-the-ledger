913f4376f2642d08042203826a6e23cca7a1cce5
bbyhateshit <j.galeta@gmail.com>
2026-04-28 09:19:17 -0400

    initialized first method that handles reading transaction data for the csv file.

--- src/main/java/com/pluralsight/FileService.java
+++ src/main/java/com/pluralsight/FileService.java
@@ -2,6 +2,10 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +18,22 @@ public class FileService {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
-       }
-}
 
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+
+                Transaction t = new Transaction(
+                        LocalDate.parse(parts[0]),
+                        LocalTime.parse(parts[1]),
+                        parts[2],
+                        parts[3],
+                        new BigDecimal(parts[4])
+                );
+                transactions.add(t);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading transactions: " + e.getMessage());
+        }
+        return transactions;
+    }
+}
