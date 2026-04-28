7171b9600ee987fb407ec979f0441966405db57d
bbyhateshit <j.galeta@gmail.com>
2026-04-28 11:01:45 -0400

    Each method deals with a specific portion of the transaction - splitting the functions into deposits and payments.

--- src/main/java/com/pluralsight/TransactionService.java
+++ src/main/java/com/pluralsight/TransactionService.java
@@ -3,6 +3,8 @@ package com.pluralsight;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TransactionService {
 
@@ -15,5 +17,31 @@ public class TransactionService {
                 amount
         );
         FileService.appendTransaction(t);
+    }
+    public static void addPayment(String description, String vendor, BigDecimal amount) {
+        Transaction t = new Transaction(
+                LocalDate.now(),
+                LocalTime.now(),
+                description,
+                vendor,
+                amount.negate()
+        );
+        FileService.appendTransaction(t);
+    }
+    public static List<Transaction> getAll() {
+        return FileService.readTransactions()
+                .stream()
+                .sorted((a, b) -> b.getDate().compareTo(a.getDate()))
+                .collect(Collectors.toList());
+    }
+    public static List<Transaction> getDeposits() {
+        return getAll().stream()
+                .filter(t -> t.getAmount().compareTo(BigDecimal.ZERO) > 0)
+                .collect(Collectors.toList());
+    }
+    public static List<Transaction> getPayments() {
+        return getAll().stream()
+                .filter(t -> t.getAmount().compareTo(BigDecimal.ZERO) < 0)
+                .collect(Collectors.toList());
     }
 }
