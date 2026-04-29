fd1c3d29d2dc885e47ad4e9c09d639dbdd91e030
bbyhateshit <j.galeta@gmail.com>
2026-04-29 08:36:05 -0400

    A method to handle each portion of the transaction to capture that specific piece of data.

--- src/main/java/com/pluralsight/ReportService.java
+++ src/main/java/com/pluralsight/ReportService.java
@@ -14,4 +14,30 @@ public class ReportService {
                 .filter(t -> YearMonth.from(t.getDate()).equals(current))
                 .collect(Collectors.toList());
     }
+    public static List<Transaction> previousMonth() {
+        YearMonth prev = YearMonth.from(LocalDate.now()).minusMonths(1);
+
+        return TransactionService.getAll().stream()
+                .filter(t -> YearMonth.from(t.getDate()).equals(prev))
+                .collect(Collectors.toList());
+    }
+    public static List<Transaction> yearToDate() {
+        int year = LocalDate.now().getYear();
+
+        return TransactionService.getAll().stream()
+                .filter(t -> t.getDate().getYear() == year)
+                .collect(Collectors.toList());
+    }
+    public static List<Transaction> previousYear() {
+        int prevYear = LocalDate.now().getYear() - 1;
+
+        return TransactionService.getAll().stream()
+                .filter(t -> t.getDate().getYear() == prevYear)
+                .collect(Collectors.toList());
+    }
+    public static List<Transaction> searchByVendor(String vendor) {
+        return TransactionService.getAll().stream()
+                .filter(t -> t.getVendor().equalsIgnoreCase(vendor))
+                .collect(Collectors.toList());
+    }
 }
