beb722e0a9a78ea8a12e945cda79bcfbe816cc0d
bbyhateshit <j.galeta@gmail.com>
2026-04-29 08:32:05 -0400

    imported my java classes and initialized my first method that handles capturing dates. The collector is to keep the data in one place - to the csv.

--- src/main/java/com/pluralsight/ReportService.java
+++ src/main/java/com/pluralsight/ReportService.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReportService {
+    public static List<Transaction> monthToDate() {
+        LocalDate now = LocalDate.now();
+        YearMonth current = YearMonth.from(now);
+
+        return TransactionService.getAll().stream()
+                .filter(t -> YearMonth.from(t.getDate()).equals(current))
+                .collect(Collectors.toList());
+    }
 }
